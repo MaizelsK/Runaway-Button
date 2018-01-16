ab840f156f183fb5c15c3ca8c964634dadeab3df
Maizels Kirill <domo.ddr@gmail.com>
2018-01-16 18:18:58 +0600

    Homework done

--- Running Button/MainWindow.xaml.cs
+++ Running Button/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace Running_Button
         {
             InitializeComponent();
         }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            int topBottom_Border = 100;
+            int leftRight_Border = 150;
+
+            Random random = new Random();
+
+            int left = (int)button.Margin.Left,
+                bottom = (int)button.Margin.Bottom;
+
+            int randomWay = random.Next(0, 4);
+
+            switch (randomWay)
+            {
+                case 0:
+                    if (left >= -this.Width + leftRight_Border)
+                        left = (int)(button.Margin.Left - button.Width*2);
+
+                    break;
+
+                case 1:
+                    if (left <= this.Width - leftRight_Border)
+                        left = (int)(button.Margin.Left + button.Width * 2);
+
+                    break;
+
+                case 2:
+                    if (bottom >= -this.Height + topBottom_Border)
+                        bottom = (int)(button.Margin.Bottom - button.Height * 2);
+
+                    break;
+
+
+                case 3:
+                    if (bottom <= this.Height - topBottom_Border)
+                        bottom = (int)(button.Margin.Bottom + button.Height * 2);
+
+                    break;
+            }
+
+            button.Margin = new Thickness(left, button.Margin.Top, button.Margin.Right, bottom);
+        }
+
+        private void Click_Button(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вы победили! :)");
+        }
     }
 }
